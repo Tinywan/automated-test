91d71e30400898b3dffb4d9ea81fb336da408d6b
Tinywan <756684177@qq.com>
2018-12-04 15:01:15 +0800

    是的撒

--- block_chain.py
+++ block_chain.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 print("This is OEM SanSheng Branch")
 print("This is Upate OEM SanSheng Branch")
+print("This is 99999999999999 OEM SanSheng Branch")
+print("This is Upate OEM SanSheng Branch")
+print("This is Upate OEM SanSheng Branch")
+print("This is Upate OEM SanSheng Branch")
+print("This is Upate OEM SanSheng Branch")
+print("This is Upate OEM SanSheng Branch")
+print("This is 1111111111 OEM SanSheng Branch")
 
